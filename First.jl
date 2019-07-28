45c8f999bcae26a0f6b7b808ff8d3c5c295212de
AlirezaKalantari <50746827+AlirezaKalantari@users.noreply.github.com>
2019-07-28 18:02:06 +0430

    new update ..

--- First.jl
+++ First.jl
@@ -16,17 +16,25 @@ using Cbc
 
 
 G=collect(1:4)    #number of generator
+j=collect(1:8)
 L=collect(1:4)    #number of load
 Y=zeros(4,4)
 J=zeros(4,4)
 E_k=[1+0im ,1+0im ,1+0im ,1+0im]
 I_cal=[1+0im ,1+0im ,1+0im ,1+0im]
-
+α_k=zeros(4,4)
+β_k=zeros(4,4)
+γ_k=zeros(4,4)
+δ_k=zeros(4,4)
+delI_r=zeros(4,1)
+delI_m=zeros(4,1)
 Pg=zeros(size(G))
 Pl=zeros(size(L))
 P_sp=zeros(size(G))
 P_cal=zeros(size(G))
 delP=zeros(size(G))
+del_I=zeros(8,1)
+J=ones(8,8)
 
 Qg=zeros(size(G))
 Ql=zeros(size(L))
@@ -65,27 +73,35 @@ G_y=real(Ybus)
 B_y=imag(Ybus)
 
 for g in G
-    P_cal[g]=V_rk[g]*real(I_cal)[g]'
-    Q_cal[g]=V_mk[g]*imag(I_cal)[g]'
+    P_cal[g]=V_rk[g]*real(I_cal)[g]'+V_mk[g]*imag(I_cal)[g]'
+    Q_cal[g]=V_mk[g]*real(I_cal)[g]'-V_rk[g]*imag(I_cal)[g]'
 
-<<<<<<< HEAD
     delP[g]=P_sp[g]-P_cal[g]
     delQ[g]=Q_sp[g]-Q_cal[g]
 
-=======
->>>>>>> b18e318ce10efb970ac3bafbf6b76a453c2733c9
+    α_k[g,g]=(Q_sp[g]*(V_rk[g]^2-V_mk[g]^2)-2*V_mk[g]*V_rk[g]*P_sp[g])/((V_mk[g]^2+V_rk[g]^2)^2)
+    β_k[g,g]=(Q_sp[g]*(V_rk[g]^2-V_mk[g]^2)+2*V_mk[g]*V_rk[g]*P_sp[g])/((V_mk[g]^2+V_rk[g]^2)^2)
+    δ_k=α_k
+    γ_k=-β_k
+
+
+    delI_r[g]=(delP[g]*V_rk[g]+V_mk[g]*delQ[g])/(V_mk[g]^2+V_rk[g]^2)
+    delI_m[g]=(delP[g]*V_mk[g]+V_rk[g]*delQ[g])/(V_mk[g]^2+V_rk[g]^2)
+
+    del_I[(2*g-1),1]=delI_r[g]
+    del_I[(2*g),1]=delI_m[g]
+
+end
+for j in j
+    for d in j
+        if j==d
+
 end
 
 
 
 
 
-
-
-<<<<<<< HEAD
-
-=======
->>>>>>> b18e318ce10efb970ac3bafbf6b76a453c2733c9
 
 
 
