54bc6a750bdf6f046f7f390e689ca70e151955d6
AlirezaKalantari <50746827+AlirezaKalantari@users.noreply.github.com>
2019-08-14 21:56:42 +0430

    ..

--- First.jl
+++ First.jl
@@ -242,8 +242,11 @@ if iter==0
     for g in G
          if del_v[g]-del_V[g]<10^(-(10)^100)
          iter=1
+
          end
     end
-
-
+        for g=1:4
+        E_k[g]=V_rk[g]+1*im(V_mk[g])
+        tete[g]=atand(V_mk[g]/V_rk[g])
+    end
 end
