354fdcf9f29b222a4ca9d6e546b833ea8120ece0
AlirezaKalantari <50746827+AlirezaKalantari@users.noreply.github.com>
2019-08-17 21:07:11 +0430

    ..

--- First.jl
+++ First.jl
@@ -240,8 +240,8 @@ if iter==0
 
        end
      for g in G
-           P_cal[g]=V_rk[g]*real(I_cal)[g]'+V_mk[g]*imag(I_cal)[g]'
-           Q_cal[g]=V_mk[g]*real(I_cal)[g]'-V_rk[g]*imag(I_cal)[g]'
+           P_cal[g]=V_rm[g]*real(I_cal)[g]'+V_rm[g+3]*imag(I_cal)[g]'
+           Q_cal[g]=V_rm[g+3]*real(I_cal)[g]'-V_rk[g]*imag(I_cal)[g]'
 
            delP[g]=P_sp[g]-P_cal[g]
            delQ[g]=Q_sp[g]-Q_cal[g]
