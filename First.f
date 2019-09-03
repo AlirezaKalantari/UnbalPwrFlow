9a5992f71cec82a7ac821e5cf5dde6929c11e4e8
AlirezaKalantari <50746827+AlirezaKalantari@users.noreply.github.com>
2019-09-03 15:51:36 +0430

    ..

--- First.f
+++ First.f
@@ -20,3 +20,12 @@ data["bus"]
 data["branch"]
 data["gen"]
 data["load"]
+
+display(data)
+PowerModels.print_summary(data)
+ct1 =PowerModels.component_table(data, "bus", "va")'
+ct2=PowerModels.component_table(data, "bus", ["vmin", "vmax"])
+ct3=PowerModels.component_table(data, "gen", ["pmin", "pmax", "qmin", "qmax"])
+ct4=PowerModels.component_table(data, "branch", ["f_bus","t_bus","br_r","br_x"])
+ct5=PowerModels.component_table(data, "load", ["load_bus","pd","qd"])
+ct6=PowerModels.component_table(data, "gen", ["gen_bus","pg","qg"])
