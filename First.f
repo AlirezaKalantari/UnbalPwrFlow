0c0914a2bbf8be194393e8e4936b3d922c7f3545
AlirezaKalantari <50746827+AlirezaKalantari@users.noreply.github.com>
2019-09-05 01:16:59 +0430

    ..

--- First.f
+++ First.f
@@ -23,7 +23,7 @@ data["load"]
 
 display(data)
 PowerModels.print_summary(data)
-ct1 =PowerModels.component_table(data, "bus", "va")'
+ct1 =PowerModels.component_table(data, "bus", "bus_type")'
 ct2=PowerModels.component_table(data, "bus", ["vmin", "vmax"])
 ct3=PowerModels.component_table(data, "gen", ["pmin", "pmax", "qmin", "qmax"])
 ct4=PowerModels.component_table(data, "branch", ["f_bus","t_bus","br_r","br_x"])
@@ -80,3 +80,32 @@ for l=1:a
 end
 
 YBUS=yb_re+(1im*yb_im)
+
+
+iter=0
+
+PV_bus=zeros(1,1)
+Iter=collect(1:1000)
+G=collect(1:(4))    #number of generator #G=collect(1:(24))
+j=collect(1:(8))
+L=collect(1:4)    #number of load
+Y=ones(4,4)
+J=zeros(4,4)
+E_k=[1+0im ,1+0im ,1+0im ,1+0im] #E_k=ones(a,1); #E_k=E_k(complex)
+I_cal=[1+0im ,1+0im ,1+0im ,1+0im]  #I_cal=ones(a,1);   ##I_cal=I_cal(complex)
+α_k=zeros(4,4)
+β_k=zeros(4,4)
+γ_k=zeros(4,4)
+δ_k=zeros(4,4)
+delI_r=zeros(4,1)
+delI_m=zeros(4,1)
+Pg=zeros(size(G))
+Pl=zeros(size(L))
+P_sp=zeros(size(G))
+P_cal=zeros(size(G))
+delP=zeros(size(G))
+del_I=zeros(8,1)
+J=ones(8,8)
+del_V=zeros(8,1)
+newdel_V=zeros(8,1)
+del_v=zeros(8,1)
