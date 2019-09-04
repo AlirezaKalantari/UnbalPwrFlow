6c31ce077ebf893724b0a1d1b559795f340af365
AlirezaKalantari <50746827+AlirezaKalantari@users.noreply.github.com>
2019-09-05 01:02:32 +0430

    ..

--- First.f
+++ First.f
@@ -33,47 +33,50 @@ ct6=PowerModels.component_table(data, "gen", ["gen_bus","pg","qg"])
 
 a=size(PowerModels.component_table(data, "bus", "bus_type"),1)
 zb_re=zeros(a,a)
+yb_re=zeros(a,a)
 
 zb_im=zeros(a,a)
 zb_im=complex(zb_im);
 
+yb_im=zeros(a,a)
+yb_im=complex(zb_im);
+
 z_RE=ones(a,a)
+
 
 q=collect(1:(a+1))
 
 for g in q
-        zb_re[ct4[g,2],ct4[g,3]]=ct4[g,4]
+        yb_re[ct4[g,2],ct4[g,3]]=(1/(ct4[g,4]+1im*ct4[g,5]))
 
 end
 
 for l=1:a
     for m=1:a
-        if zb_re[l,m]!==0
-            zb_re[m,l]=zb_re[l,m]
+        if yb_re[l,m]!==0
+            yb_re[m,l]=yb_re[l,m]
         end
     end
 end
 
 for l=1:a
-    zb_re[l,l]=-(zb_re[l,1]+zb_re[l,2]+zb_re[l,3]+zb_re[l,4]+zb_re[l,5])
+    yb_re[l,l]=(yb_re[l,1]+yb_re[l,2]+yb_re[l,3]+yb_re[l,4]+yb_re[l,5])
 end
 
 for g in q
-    zb_im[ct4[g,2],ct4[g,3]]=ct4[g,5]
+    yb_im[ct4[g,2],ct4[g,3]]=-imag(1/(ct4[g,4]+1im*ct4[g,5]))
 end
 
 for l=1:a
     for m=1:a
-        if zb_im[l,m]!==0
-            zb_im[m,l]=zb_im[l,m]
-
+        if yb_im[l,m]!==0
+            yb_im[m,l]=yb_im[l,m]
         end
     end
 end
 
 for l=1:a
-    zb_im[l,l]=-(zb_im[l,1]+zb_im[l,2]+zb_im[l,3]+zb_im[l,4]+zb_im[l,5])
+    yb_im[l,l]=(yb_im[l,1]+yb_im[l,2]+yb_im[l,3]+yb_im[l,4]+yb_im[l,5])
 end
 
-ZBUS=zb_re+(1im*zb_im)
-YBUS=(ZBUS)^-1
+YBUS=yb_re+(1im*yb_im)
