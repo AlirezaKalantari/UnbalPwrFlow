99bbb6d263eae82c1312b46c86ae64b036c3d3f1
AlirezaKalantari <50746827+AlirezaKalantari@users.noreply.github.com>
2019-09-18 22:17:18 +0430

    ..f14

--- First.f
+++ First.f
@@ -265,7 +265,7 @@ if iter==0
                J[s,c+3]=real(YBUS[s-1,b])-α_k[s-1,s-1]
                s=s-1
                delI_m[s]=V_rm[s+3,1]/(V_rm[s,1]^2+V_rm[s+3*a,1]^2)
-               delI_r[s]=V_rm[s,1]/(V_rm[s,1]^2+V_rm[s+3,1]^2)
+               delI_r[s]=V_rm[s,1]/(V_rm[s,1]^2+V_rm[s+3*a,1]^2)
                del_I[(2*b-1),1]=delI_m[s]
                del_I[(2*b),1]=delI_r[s]
 
@@ -285,4 +285,4 @@ for g in G_1
 end
 
 
-println("salam saeed jan, Because there was no bus in terminal, I must to travel this night and start remaining work")
+#println("salam saeed jan, Because there was no bus in terminal, I must to travel this night and start remaining work")
