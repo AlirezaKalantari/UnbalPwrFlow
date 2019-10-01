d03168f41e35fe7c5044183cb16321f3d078cd73
AlirezaKalantari <50746827+AlirezaKalantari@users.noreply.github.com>
2019-10-01 22:55:55 +0330

    ..

--- First.f
+++ First.f
@@ -259,7 +259,8 @@ if iter==0
                 if YBUS[c,b]!==0.0
                     if c==b
                         for d=1:3
-                            J[6*(c-1)+d,6*(b-1)+d]=real(YBUS[c,b])-(β_k[3*(b-1)+d,3*(b-1)+d])-(imag(YBUS[c,b])-(α_k[3*(b-1)+d,3*(b-1)+d]))*(V_rm[c+3*a,1]/V_rm[c,1])
+                            J[6*(c-1)+d,6*(b-1)+d]=real(YBUS[c,b])-(β_k[3*(b-1)+d,3*(b-1)+d])-(imag(YBUS[c,b])-(α_k[3*(b-1)+d,3*(b-1)+d]))*(V_rm[b+3*a,1]/V_rm[c,1])
+                            J[6*(c-1)+d,6*(b-1)+d+3]=V_rm[b,1]/(V_rmbs,1]^2+V_rm[b+3*a,1]^2)
                             #J[6*(b-1)+d,6*(c-1)+d+3]=real(YBUS[b,c])
                             #J[6*(b-1)+d+3,6*(c-1)+d]=imag(YBUS[b,c])
                             #J[6*(b-1)+d+3,6*(c-1)+d+3]=imag(YBUS[b,c])
