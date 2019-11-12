a3dd8a2d10a618d6906d9e2fba1477553e403705
AlirezaKalantari <50746827+AlirezaKalantari@users.noreply.github.com>
2019-11-12 16:42:53 +0330

    ..

--- First.f
+++ First.f
@@ -330,6 +330,9 @@ end
 #delI_m=zeros(3*a,t)
 #I_cal=complex(ones(6*a,1),24)
 #β_k=diagm(0=>ones(3*a),t)
+#  α_k=diagm(0=>ones(3*a),t)   
+
+
 
 #=for t=1:24
     for i=1:(size(ct5,1))       #active & reactive load of power of any time
