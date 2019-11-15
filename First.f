beaa0ed0d12c2d6169e487fb8fc20c24e890d643
AlirezaKalantari <50746827+AlirezaKalantari@users.noreply.github.com>
2019-11-15 21:56:16 +0330

    ,,

--- First.f
+++ First.f
@@ -381,8 +381,8 @@ end=#
 
 #=for b=1:a
     for w=1:3                                                   #equation 13
-        if  Ql_1[6*(b-1)+w,t]!==0 || Ql_1[6*(b-1)+w+3]!==0
-
+        if  Ql_1[6*(b-1)+w,t]!==0 || Ql_1[6*(b-1)+w+3,t]!==0
+        J[6*(b-1)+w,6*(b-1)+w]=imag(YBUS[b,b])-(α_k[3*(b-1)+w,3*(b-1)+w])       #equation 14
         end
     end
 end=#
