37a6b86f8e5023fd82ce0aeaee5690b400a8238e
AlirezaKalantari <50746827+AlirezaKalantari@users.noreply.github.com>
2019-11-16 18:19:55 +0330

    ,,

--- First.f
+++ First.f
@@ -395,6 +395,7 @@ end=#
 #=for d=1:3
     if b!==a &&  (Ql_1[6*(b-1)+d]!==0 || Ql_1[6*(b-1)+d+3]!==0)
         J[6*(b-1)+d,6*(c-1)+d]=imag(YBUS[b,c])
+        J[6*(b-1)+d,6*(c-1)+d+3]=real(YBUS[b,c])
     end
 end=#
 
